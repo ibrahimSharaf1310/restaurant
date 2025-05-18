35f3e4f4ab4a3be4b95984184f7a1569e449a977
ibrahimSharaf1310 <167881843+ibrahimSharaf1310@users.noreply.github.com>
2025-05-19 00:41:28 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let menu =document.querySelector('#menu-bar');
+let navbar =document.querySelector('.navbar');
+menu.onclick=function()
+{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+
+}
+window.onscroll=function()
+{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    
+}
